96d675bebeb3d6859db66cc47cbde91c07aae1af
KwatME <kwatme8@gmail.com>
2018-06-25 12:29:41 -0700

    0

--- code/environment.py
+++ code/environment.py
@@ -8,9 +8,12 @@ from warnings import filterwarnings
 import ccal
 import numpy as np
 import pandas as pd
+from plotly.offline import init_notebook_mode
 
 from compute_correlation_distance import compute_correlation_distance
 from make_feature_dicts import make_feature_dicts
 from make_paths import make_paths
 
 filterwarnings('ignore')
+
+init_notebook_mode(connected=True)
