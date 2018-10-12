a79eadb7b165f40fd50265cc8521e84112b69e40
KwatME <kwatme8@gmail.com>
2018-10-12 15:11:52 -0700

    steph

--- code/make_feature_dicts.py
+++ code/make_feature_dicts.py
@@ -1,82 +1,57 @@
-from ccal import make_membership_df_from_categorical_series
-from pandas import read_table
+import gzip
+import pickle
 
-from make_feature_dicts_from_pickle_gz import make_feature_dicts_from_pickle_gz
+import ccal
+import pandas as pd
 
 
-def make_feature_dicts(
-        load_ccle=False,
-        load_tcga=False,
-):
+def make_feature_dicts():
 
-    feature_dicts = {}
-
-    if load_ccle:
-
-        feature_dicts.update(
-            make_feature_dicts_from_pickle_gz(
-                '../data/ccle.pickle.gz',
-                (
-                    #  'Information',
-                    'Mutation',
-                    'Mutational Signature',
-                    'CNV',
-                    'Methylation',
-                    'miRNA',
-                    'RNA',
-                    'Gene Set',
-                    'Protein',
-                    'Metabolite',
-                    'Achilles RNAi',
-                    'Achilles CRISPR',
-                    'NP24 Compound',
-                    'CTRP Compound',
-                ),
-                {},
-            ))
-
-    if load_tcga:
-
-        feature_dicts.update(
-            make_feature_dicts_from_pickle_gz(
-                '../data/tcga.pickle.gz',
-                (
-                    'Categorical Information',
-                    'Continuous Information',
-                    'Mutation',
-                    'Mutational Signature',
-                    'CNV',
-                    'Methylation',
-                    'miRNA',
-                    'RNA',
-                    'Gene Set',
-                    'Protein',
-                    'Immune Signature',
-                ),
-                {},
-            ))
-
-    cell_line_state = read_table(
-        '../data/pablo/states.tsv',
-        index_col=0,
-        header=None,
-        squeeze=True,
+    pickle_gz_file_path = '../data/tcga.pickle.gz'
+    
+    feature_groups = (
+        'Categorical Information',
+        'Continuous Information',
+        'Mutation',
+        'Mutational Signature',
+        'CNV',
+        'Methylation',
+        'miRNA',
+        'RNA',
+        'Gene Set',
+        'Protein',
+        'Immune Signature',
     )
+    
+    feature_group_features = {}
+    
+    with gzip.open(pickle_gz_file_path) as pickle_gz_file:
 
-    cell_line_state.name = 'State'
+        feature_dicts = pickle.load(pickle_gz_file)
 
-    cell_line_state.index.name = 'Cell Line'
-
-    state_x_cell_line = make_membership_df_from_categorical_series(
-        cell_line_state)
-
-    state_x_cell_line.index = ('State {}'.format(i)
-                               for i in state_x_cell_line.index)
-
-    feature_dicts['Pablo State'] = {
-        'df': state_x_cell_line.append(cell_line_state),
-        'data_type': 'categorical',
-        'emphasis': 'high',
+    feature_dicts = {
+        feature_group: feature_dicts[feature_group]
+        for feature_group in feature_groups
     }
 
+    for feature_group, features in feature_group_features.items():
+
+        for feature in features:
+
+            feature_dicts['{} {}'.format(
+                feature_group,
+                feature,
+            )] = {
+                'df':
+                ccal.make_membership_df_from_categorical_series(
+                    feature_dicts[feature_group]['df'].loc[feature]).astype(
+                        int),
+                'data_type':
+                'binary',
+                'emphasis':
+                'high',
+            } 
+
     return feature_dicts
+
+
