1db12a1a7cc5d7ce0c82633a9297d6913a4355a2
KwatME <kwatme8@gmail.com>
2018-11-11 10:13:34 -0800

    Git sync (0)

--- code/environment.py
+++ code/environment.py
@@ -1,10 +1,6 @@
-import gzip
 import itertools
 import json
 import os
-import pickle
-import sys
-from warnings import filterwarnings
 
 import ccal
 import numpy as np
@@ -13,7 +9,5 @@ from plotly.offline import init_notebook_mode
 
 from make_feature_dicts import make_feature_dicts
 from path import path
-
-filterwarnings('ignore')
 
 init_notebook_mode(connected=True)
