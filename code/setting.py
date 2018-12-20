c60649102c716b9b9aca0532ef956f5cfd76ad5d
KwatME <kwatme8@gmail.com>
2018-12-20 01:46:42 -0800

    Git sync (0)

--- code/setting.py
+++ code/setting.py
@@ -4,22 +4,24 @@ from make_path_dict import make_path_dict
 # ==============================================================================
 # Please tell me about your feature-by-sample data.
 # ==============================================================================
-FEATURE_X_SAMPLE_FILE_PATH = "/media/kwatme/CarrotCake/data/cell_line/ctrp_select/ctrp_2g__compound_x_cell_line.CENTRAL_NERVOUS_SYSTEM.FIBROBLAST.MATCHED_NORMAL_TISSUE.tsv"
+FEATURE_X_SAMPLE_FILE_PATH = "/home/kwatme/project/katoiii/KatoIII/gene_x_delegate.tsv"
 
-FEATURE_X_SAMPLE_ALIAS = "Compound CTRP 2g Central Nervous System (18.12.14)"
+FEATURE_X_SAMPLE_ALIAS = "scRNA KatoIII (18.12.19)"
 
-FEATURE_ALIAS = "Compound"
+FEATURE_ALIAS = "Gene"
 
-SAMPLE_ALIAS = "Cell Line"
+SAMPLE_ALIAS = "Delegate"
 
-FEATURE_X_SAMPLE_VALUE_NAME = "AUC"
+FEATURE_X_SAMPLE_VALUE_NAME = "Expression"
 
 HIGHLIGHT_JSON_FILE_PATH = "../code/highlight.json"
 
-OUTPUT_DIRECTORY_PATH = "/home/kwatme/project/{}".format(FEATURE_X_SAMPLE_ALIAS)
+OUTPUT_DIRECTORY_PATH = "/home/kwatme/project/katoiii/KatoIII/{}".format(
+    FEATURE_X_SAMPLE_ALIAS
+)
 
-# PLOTLY_DIRECTORY_PATH = None
-PLOTLY_DIRECTORY_PATH = "Cellular Context/{}".format(FEATURE_X_SAMPLE_ALIAS)
+PLOTLY_DIRECTORY_PATH = None
+# PLOTLY_DIRECTORY_PATH = "Cellular Context/{}".format(FEATURE_X_SAMPLE_ALIAS)
 
 # ==============================================================================
 # How do you want to plot?
@@ -37,7 +39,7 @@ PLOT_STD = 2.4
 # ==============================================================================
 # How many thread(s) are you willing to use simultaneously?
 # ==============================================================================
-MAX_N_JOB = 1
+MAX_N_JOB = 38
 
 # ==============================================================================
 # How do you want to pre-process the data?
@@ -52,15 +54,15 @@ DROP_AXIS = None
 
 MAX_NA = None
 
-MIN_N_NOT_NA_UNIQUE_VALUE = 1
+MIN_N_NOT_NA_UNIQUE_VALUE = None
 
 SHIFT_AS_NECESSARY_TO_ACHIEVE_MIN_BEFORE_LOGGING = None
 
 LOG_BASE = None
 
-NORMALIZATION_AXIS = None
+NORMALIZATION_AXIS = 0
 
-NORMALIZATION_METHOD = "-0-"
+NORMALIZATION_METHOD = "sum"
 
 CLIP_MIN = None
 
@@ -69,64 +71,47 @@ CLIP_MAX = None
 # ==============================================================================
 # Do you want to select only good gene symbols?
 # ==============================================================================
-SELECT_GENE_SYMBOL = False
+SELECT_GENE_SYMBOL = True
 
 # ==============================================================================
 # What are some elements you want to focus on?
 # ==============================================================================
-FEATURES_TO_PEEK = (
-    "1S,3R-RSL-3",
-    "austocystin D",
-    "AZD4547",
-    "erastin",
-    "ML162",
-    "ML210",
-    "nintedanib",
-    "selumetinib",
-)
+FEATURES_TO_PEEK = ("FGFR1", "FGFR1OP", "FGFR1OP2", "FGFR2", "FGFR4", "FGFRL1")
 
-SAMPLES_TO_PEEK = (
-    "HMCB_SKIN",
-    "SKMEL31_SKIN",
-    "CAL51_BREAST",
-    "CAL148_BREAST",
-    "HMC18_BREAST",
-    "KPL1_BREAST",
-    "MDAMB468_BREAST",
-    "BEN_LUNG",
-    "AM38_CENTRAL_NERVOUS_SYSTEM",
-    "DAOY_CENTRAL_NERVOUS_SYSTEM",
-    "SF126_CENTRAL_NERVOUS_SYSTEM",
-)
+SAMPLES_TO_PEEK = ()
 
 # ==============================================================================
 # How do you want to normalize signal?
 # ==============================================================================
+RAW_SIGNAL_NORMALIZATION_AXIS = None
+
 RAW_SIGNAL_NORMALIZATION_METHOD = "0-1"
+
+CONTEXT_SIGNAL_NORMALIZATION_AXIS = None
 
 CONTEXT_SIGNAL_NORMALIZATION_METHOD = "0-1"
 
 # ==============================================================================
 # Which element type(s) do you want use for signal?
 # ==============================================================================
-ELEMENT_TYPES = ("feature", "sample")
+ELEMENT_TYPES = ("feature",)
 
 # ==============================================================================
 # Which context(s) do you want use for signal?
 # ==============================================================================
-CONTEXTS = ("negative",)
+CONTEXTS = ("negative", "positive")
 
 # ==============================================================================
 # Do you want to select elements automatically when making signal?
 # ==============================================================================
-SELECT_FEATURE_AUTOMATICALLY = False
+SELECT_FEATURE_AUTOMATICALLY = True
 
 SELECT_SAMPLE_AUTOMATICALLY = False
 
 # ==============================================================================
 # How do you want to factorize the signal?
 # ==============================================================================
-NMF_KS = tuple(range(2, 10))
+NMF_KS = tuple(range(2, 20))
 
 NMF_K = 5
 
@@ -158,9 +143,9 @@ ELEMENT_ENTROPY_QUANTILE = 1
 # ==============================================================================
 GPS_MAP_W_PULL_POWER = 1.6
 
-GPS_MAP_W_ELEMENT_MARKER_SIZE = 16
+GPS_MAP_W_ELEMENT_MARKER_SIZE = 8
 
-GPS_MAP_W_BANDWIDTH_FACTOR = 3.2
+GPS_MAP_W_BANDWIDTH_FACTOR = 1
 
 # ==============================================================================
 # How do you want to make sample GPS Map?
@@ -169,7 +154,7 @@ GPS_MAP_H_PULL_POWER = 1.6
 
 GPS_MAP_H_ELEMENT_MARKER_SIZE = 24
 
-GPS_MAP_H_BANDWIDTH_FACTOR = 2.4
+GPS_MAP_H_BANDWIDTH_FACTOR = 1
 
 # ==============================================================================
 # I'm making output paths based on these settings ...
