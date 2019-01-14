df07549bcaa1a0440fef47639448838247813683
KwatME <kwatme8@gmail.com>
2019-01-14 15:45:14 -0800

    get

--- code/environment.py
+++ code/environment.py
@@ -2,12 +2,12 @@ import itertools
 import os
 import warnings
 
+import ccal
 import numpy as np
 import pandas as pd
 import plotly
 import yaml
 
-import ccal
 from make_path_dict import make_path_dict
 
 warnings.filterwarnings("ignore")
