28036d970a32c0ecd7420eef24c510a7b0da5e8e
KwatME <kwatme8@gmail.com>
2019-04-08 20:14:35 -0700

    sync

--- code/make_path_dict.py
+++ code/make_path_dict.py
@@ -1,92 +1,101 @@
-from os.path import expanduser
+from os.path import join
 
-from ccal import establish_path
+from ccal import establish_path, get_absolute_path
 
 
 def make_path_dict(setting):
 
-    output_directory_path = expanduser(setting["output_directory_path"])
+    output_directory_path = get_absolute_path(setting["output_directory_path"])
 
     path_dict = {}
 
     name = "feature_x_sample.processed.tsv"
 
-    path_dict[name] = "{}/{}".format(output_directory_path, name)
+    path_dict[name] = join(output_directory_path, name)
 
     for name in ("feature_x_fit_parameter.tsv", "sample_x_fit_parameter.tsv"):
 
-        path_dict[name] = "{}/fit/{}".format(output_directory_path, name)
+        path_dict[name] = join(output_directory_path, "fit", name)
 
     for name in (
         "feature_x_sample.feature_context.tsv",
         "feature_x_sample.sample_context.tsv",
     ):
 
-        path_dict[name] = "{}/context/{}".format(output_directory_path, name)
+        path_dict[name] = join(output_directory_path, "context", name)
 
     for name in ("feature_x_sample.signal.tsv", "nmf/"):
 
-        path_dict[name] = "{}/signal/{}/{}".format(
-            output_directory_path, setting["signal_type"], name
+        path_dict[name] = join(
+            output_directory_path, "signal", setting["signal_type"], name
         )
+
+    nmf_k = str(setting["nmf_k"])
 
     for w_or_h in ("w", "h"):
 
         for name in ("{}.tsv".format(w_or_h), "{}/".format(w_or_h)):
 
-            path_dict[name] = "{}/signal/{}/nmf/{}/{}".format(
-                output_directory_path, setting["signal_type"], setting["nmf_k"], name
+            path_dict[name] = join(
+                output_directory_path,
+                "signal",
+                setting["signal_type"],
+                "nmf",
+                nmf_k,
+                name,
             )
 
         for name in ("match/", "gps_map/", "hcc/", "summary/"):
 
-            path_dict[
-                "{}|{}".format(w_or_h, name)
-            ] = "{}/signal/{}/nmf/{}/{}/{}".format(
+            path_dict["{}|{}".format(w_or_h, name)] = join(
                 output_directory_path,
+                "signal",
                 setting["signal_type"],
-                setting["nmf_k"],
+                "nmf",
+                nmf_k,
                 w_or_h,
                 name,
             )
 
-        hcc_k = setting["{}_hcc_k".format(w_or_h)]
+        hcc_k = str(setting["{}_hcc_k".format(w_or_h)])
 
         name = "cluster_x_column.tsv"
 
-        path_dict[
-            "{}|{}".format(w_or_h, name)
-        ] = "{}/signal/{}/nmf/{}/{}/hcc/{}/{}".format(
+        path_dict["{}|{}".format(w_or_h, name)] = join(
             output_directory_path,
+            "signal",
             setting["signal_type"],
-            setting["nmf_k"],
+            "nmf",
+            nmf_k,
             w_or_h,
+            "hcc",
             hcc_k,
             name,
         )
 
         for name in ("match/", "gps_map/", "comparison/"):
 
-            path_dict[
-                "{}|hcc|{}".format(w_or_h, name)
-            ] = "{}/signal/{}/nmf/{}/{}/hcc/{}/{}".format(
+            path_dict["{}|hcc|{}".format(w_or_h, name)] = join(
                 output_directory_path,
+                "signal",
                 setting["signal_type"],
-                setting["nmf_k"],
+                "nmf",
+                nmf_k,
                 w_or_h,
+                "hcc",
                 hcc_k,
                 name,
             )
 
     name = "gps_map.pickle.gz"
 
-    path_dict[name] = "{}/signal/{}/nmf/{}/{}".format(
-        output_directory_path, setting["signal_type"], setting["nmf_k"], name
+    path_dict[name] = join(
+        output_directory_path, "signal", setting["signal_type"], "nmf", nmf_k, name
     )
 
     for name in ("summary/", "infer/"):
 
-        path_dict[name] = "{}/{}".format(output_directory_path, name)
+        path_dict[name] = join(output_directory_path, name)
 
     for name, path in path_dict.items():
 
@@ -118,12 +127,13 @@ def make_path_dict(setting):
 
         else:
 
-            plotly_w_or_h_match_directory_path = "{}/{} Match".format(
-                setting["plotly_directory_path"], element_type_title
+            plotly_w_or_h_match_directory_path = join(
+                setting["plotly_directory_path"], "{} Match".format(element_type_title)
             )
 
-            plotly_w_or_h_gps_directory_path = "{}/{} GPS Map".format(
-                setting["plotly_directory_path"], element_type_title
+            plotly_w_or_h_gps_directory_path = join(
+                setting["plotly_directory_path"],
+                "{} GPS Map".format(element_type_title),
             )
 
         path_dict[
