88cc45ac8d543c12f3c778c15dc1c460cd2a6120
KwatME <kwatme8@gmail.com>
2019-04-17 21:38:58 -0700

    CCAL 1.1.0?

--- code/environment.py
+++ code/environment.py
@@ -3,10 +3,10 @@ import os
 
 import numpy as np
 import pandas as pd
-import plotly as pl
+import plotly
 import yaml
 
 import ccal
 from make_path_dict import make_path_dict
 
-pl.offline.init_notebook_mode(connected=True)
+plotly.offline.init_notebook_mode(connected=True)
