278d96498154c90088ae441b01d102088485964c
KwatME <kwatme8@gmail.com>
2020-01-17 14:39:43 -0800

    pom

--- code/read_ws_hs.py
+++ code/read_ws_hs.py
@@ -3,25 +3,27 @@ from pandas import read_csv
 
 def read_ws_hs(mf_directory_path, model_mode, model_data_dicts):
 
-    n_data_dicts = len(model_data_dicts)
+    n_data_dict = len(model_data_dicts)
 
     if model_mode == "range":
 
         n_w = 1
 
-        n_h = n_data_dicts
+        n_h = n_data_dict
 
     elif model_mode == "deep":
 
-        n_w = n_data_dicts
+        n_w = n_data_dict
 
         n_h = 1
 
     ws = []
 
-    for i in range(n_w):
+    for w_index in range(n_w):
 
-        w = read_csv("{}/w{}.tsv".format(mf_directory_path, i), sep="\t", index_col=0)
+        w = read_csv(
+            "{}/w{}.tsv".format(mf_directory_path, w_index), sep="\t", index_col=0
+        )
 
         w.columns.name = "Factor"
 
@@ -29,11 +31,13 @@ def read_ws_hs(mf_directory_path, model_mode, model_data_dicts):
 
     hs = []
 
-    for i in range(n_h):
+    for h_index in range(n_h):
 
-        h = read_csv("{}/h{}.tsv".format(mf_directory_path, i), sep="\t", index_col=0)
+        h = read_csv(
+            "{}/h{}.tsv".format(mf_directory_path, h_index), sep="\t", index_col=0
+        )
 
-        h.columns.name = model_data_dicts[i]["axis1_name"]
+        h.columns.name = model_data_dicts[h_index]["axis1_name"]
 
         hs.append(h)
 
