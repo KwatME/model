abf75d3a96e4daa929e3d4a227a54f11a571d364
KwatME <kwat.me@icloud.com>
2021-01-14 14:00:53 -0800

    log

--- code/__init__.py
+++ code/__init__.py
@@ -1,10 +1,9 @@
 import os
 import re
 
+import kraft
 import numpy as np
 import pandas as pd
 from read_factorization import read_factorization
-
-import kraft
 
 SETTING = kraft.json.read("setting.json")
