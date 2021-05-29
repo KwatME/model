8745d31c1908167de085b9c2d5ec177a59be2eae
KwatME <kwat.me@icloud.com>
2021-05-29 11:52:30 -0700

    ,

--- code/read_factorization.py
+++ code/read_factorization.py
@@ -1,7 +1,13 @@
-from pandas import read_csv
+from pandas import (
+    read_csv,
+)
 
 
-def read_factorization(directory_path, model_mode, model_data_):
+def read_factorization(
+    directory_path,
+    model_mode,
+    model_data_,
+):
 
     data_n = len(model_data_)
 
@@ -20,12 +26,26 @@ def read_factorization(directory_path, model_mode, model_data_):
     path_template = "{}{{}}_{{}}.tsv".format(directory_path)
 
     w_df_ = tuple(
-        read_csv(path_template.format("w", index), sep="\t", index_col=0)
+        read_csv(
+            path_template.format(
+                "w",
+                index,
+            ),
+            sep="\t",
+            index_col=0,
+        )
         for index in range(w_n)
     )
 
     h_df_ = tuple(
-        read_csv(path_template.format("h", index), sep="\t", index_col=0)
+        read_csv(
+            path_template.format(
+                "h",
+                index,
+            ),
+            sep="\t",
+            index_col=0,
+        )
         for index in range(h_n)
     )
 
@@ -33,8 +53,14 @@ def read_factorization(directory_path, model_mode, model_data_):
 
         df.columns.name = "Factor"
 
-    for df, data in zip(h_df_, model_data_):
+    for (df, data,) in zip(
+        h_df_,
+        model_data_,
+    ):
 
         df.columns.name = data["axis_1_name"]
 
-    return w_df_, h_df_
+    return (
+        w_df_,
+        h_df_,
+    )
